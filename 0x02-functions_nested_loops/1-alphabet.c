b9cb8bd185e31fe9dc20e0d79088d344b8dc458a
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-21 20:43:00 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void) 
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
