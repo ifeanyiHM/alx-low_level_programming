e6096539c3d1dadc38a0c19e066a743a53262545
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-22 09:38:14 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
 		return (1);
 	}
 
 	if (n == 0)
 	{
 		_putchar(0);
-		_putchar(',');
 		return (0);
 	}
 
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(';');
 		return(-1);
 	}
-	return (0);
 }
