d7e0892ec385ad4ffe084b573fc546e1192c1960
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-22 09:30:26 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_sign -> print if n is greater than 0 or not
+* @n: the character for check
+* Return: returns 1 or 0 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar(0);
+		_putchar(',');
+		return (0);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(';');
+		return(-1);
+	}
+}
