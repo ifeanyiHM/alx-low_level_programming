00f9a1cf48c4847238e3fa9594c9dda3a4a38090
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-23 19:39:36 +0000

    2-mul.c  3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - prints from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+	while (n <= 9)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
