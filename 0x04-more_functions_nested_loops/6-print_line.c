7c35b086ac999e076d7aa6a3bc1feb3c5e9eaa03
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-23 21:16:18 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,16 @@ void print_line(int n)
 {
 	int x;
 
-	for (x = 0; x <= n; x++)
-	{
 		if (n <= 0)
 		{
 			_putchar('\n');
 		}
 		else
 		{
-			_putchar(95);
+			for (x = 0; x <= n; x++)
+			{
+				_putchar(95);
+			}
+			_putchar('\n');
 		}
-	}
-	_putchar('\n');
 }
