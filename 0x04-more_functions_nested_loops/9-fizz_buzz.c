feb699233700b2b1c8aebe90407d116e521703e7
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-24 16:11:05 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - prints the number 1 10 100
+* Return: return 0
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 100)
+	{
+		n++;
+
+		if (n % (3 * 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
