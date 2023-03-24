7ebd6f63dd085c76bb6391e43664e2171e074d93
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-24 18:09:41 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: parameter
+* Return: return empty
+*/
+
+void print_number(int n)
+{
+	_putchar(n);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,3 +19,5 @@ void print_square(int size);
 int main(void);
 
 void print_triangle(int size);
+
+void print_number(int n);
