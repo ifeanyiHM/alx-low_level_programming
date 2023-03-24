364dbf2864dcfb9e382fff65761ac195c9ad7403
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-03-24 14:33:17 +0000

    ./8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_square - print square using #
+* @size: parameter
+* Return: return empty
+*/
+
+void print_square(int size)
+{
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int n, j;
+
+		for (n = 0; n <= size; n++)
+		{
+			_putchar('\n');
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
