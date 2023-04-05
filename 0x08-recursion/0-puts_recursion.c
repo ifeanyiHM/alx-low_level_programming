7b5e45b24c5a12ea26d8223805e3338c40310ff8
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-04-05 12:47:51 +0000

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - A function that prints a string
+* @s: parameter
+* Return: return empty
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+
